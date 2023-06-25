6838755066a2c6a53573396871fe2a3d0e5c106c
Chufretalas <marcobene.contact@gmail.com>
2023-06-25 14:40:44 -0300

    styled the new pet form

--- src/components/NewPetDialog/NewPetDialog.tsx
+++ src/components/NewPetDialog/NewPetDialog.tsx
@@ -1,6 +1,7 @@
 import { RefObject, useEffect, useRef } from "react"
 import DefaultFieldset from "../DefaultFieldset/DefaultFieldset"
 import OrangeButton from "../OrangeButton/OrangeButton"
+import styles from "./NewPetDialog.module.css"
 
 export default function NewPetDialog({ isOpened, onClose }: { isOpened: boolean, onClose: () => void }) {
     const ref = useRef<HTMLDialogElement>(null)
@@ -15,16 +16,18 @@ export default function NewPetDialog({ isOpened, onClose }: { isOpened: boolean,
 
     async function handleForm(fd: FormData) {
         console.log(fd.get("birthday"))
+        console.log(fd.get("pet_size"))
         //TODO: validade the form data, such as the birthday that cannot be in the future
     }
 
     return (
-        <dialog ref={ref}>
+        <dialog ref={ref} className={styles.dialog}>
             <button onClick={() => {
                 onClose()
                 ref.current?.close()
-            }}>✖️</button>
-            <form action={handleForm}>
+            }}
+                className={styles.close_button}>✖️</button>
+            <form action={handleForm} className={styles.form}>
                 <DefaultFieldset>
                     <label htmlFor="name">Name</label>
                     <input type="text" placeholder="The name of the pet"
@@ -47,30 +50,12 @@ export default function NewPetDialog({ isOpened, onClose }: { isOpened: boolean,
                 <DefaultFieldset>
                     <span>How big is this pet?</span>
                     <div>
-                        <div>
-                            <input type="radio" name="pet_size" id="small" value="small" defaultChecked={true} required />
-                            <label htmlFor="small">small</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="pet_size" id="medium/small" value="medium/small" required />
-                            <label htmlFor="medium/small">medium/small</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="pet_size" id="medium" value="medium" required />
-                            <label htmlFor="medium">medium</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="pet_size" id="medium/large" value="medium/large" required />
-                            <label htmlFor="medium/large">medium/large</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="pet_size" id="large" value="large" required />
-                            <label htmlFor="large">large</label>
-                        </div>
-                        <div>
-                            <input type="radio" name="pet_size" id="very_big" value="very big" required />
-                            <label htmlFor="very_big">very big</label>
-                        </div>
+                        <PetSizeRadioSet id="small" value="small" defaultChecked={true} />
+                        <PetSizeRadioSet id="medium/small" value="medium/small" />
+                        <PetSizeRadioSet id="medium" value="medium" />
+                        <PetSizeRadioSet id="medium/large" value="medium/large" />
+                        <PetSizeRadioSet id="large" value="large" />
+                        <PetSizeRadioSet id="very_big" value="very big" />
                     </div>
                 </DefaultFieldset>
                 {/* <DefaultFieldset> // TODO: pet img maybe sometime
@@ -83,9 +68,21 @@ export default function NewPetDialog({ isOpened, onClose }: { isOpened: boolean,
                     <input type="personality" placeholder="Describe it in one or two words"
                         id="personality" name="personality" required maxLength={40} />
                 </DefaultFieldset>
-                <OrangeButton type="submit">Confirm</OrangeButton>
+                <OrangeButton type="submit" className={styles.submit_button}>Confirm</OrangeButton>
             </form>
 
         </dialog>
+    )
+}
+
+function PetSizeRadioSet({ id, value, defaultChecked }: { id: string, value: string, defaultChecked?: boolean }) {
+    return (
+        <div>
+            <input type="radio" name="pet_size"
+                id={id} value={value}
+                defaultChecked={defaultChecked === undefined ? false : defaultChecked}
+                required />
+            <label htmlFor={id}>{value}</label>
+        </div>
     )
 }
